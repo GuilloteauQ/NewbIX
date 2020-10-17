3c6209ddb2630f8078bf02eef6ed8f445a015ed5
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-10-17 18:34:05 +0000

    default.nix

--- default.nix
+++ default.nix
@@ -3,8 +3,9 @@ with import <nixpkgs> { };
 rustPlatform.buildRustPackage rec {
   name = "sifa";
   version = "1.0";
-  src = ./.;
-  buildInputs = [ ];
+  # src = ./.;
+  src = fetchTarball("https://github.com/GuilloteauQ/SIFA/tarball/main");
+  buildInputs = [ cargo ];
 
   checkPhase = "";
   cargoSha256 = "sha256:0ilva42mnqkbpwp646zwrahlpnkfr3q4b7xcjyi2zwgsw6fqbdcx";
