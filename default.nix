fe10545dbcc235b3caec49cba14d0beccd5c52d2
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-11-03 13:29:43 +0000

    Finally the correct hash ?

--- default.nix
+++ default.nix
@@ -8,7 +8,8 @@ rustPlatform.buildRustPackage rec {
   buildInputs = [ cargo ];
 
   checkPhase = "";
-  cargoSha256 = "sha256:0z1m63xi0fll1gg2x7ynykfis8n3zydwavf9pjxzcsh6c8pa8i77";
+  # cargoSha256 = "sha256:0z1m63xi0fll1gg2x7ynykfis8n3zydwavf9pjxzcsh6c8pa8i77";
+  cargoSha256 = "sha256:0fsg7w2cwq9mv2467d21p81yb60j0w0a1ap07aq5x3fj9bd9y61r";
 
 }
 
